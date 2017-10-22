6b537d52534391fd60ad4e731b186c69dcce3ff7
okunishinishi <okunishinishi@gmail.com>
2017-10-22 09:49:53 +0900

    Versionup

--- test/json_driver_test.js
+++ test/json_driver_test.js
@@ -5,107 +5,112 @@
 'use strict'
 
 const JsonDriver = require('../lib/json_driver.js')
-const { ok, equal, deepEqual } = require('assert')
+const {ok, equal, deepEqual} = require('assert')
 const clayLump = require('clay-lump')
 const rimraf = require('rimraf')
 const clayDriverTests = require('clay-driver-tests')
-const co = require('co')
 
 describe('json-driver', function () {
-  this.timeout(30000)
+  this.timeout(50000)
 
-  before(() => co(function * () {
+  before(async () => {
 
-  }))
+  })
 
-  after(() => co(function * () {
+  after(async () => {
 
-  }))
+  })
 
-  it('Json driver', () => co(function * () {
-    let driver = new JsonDriver(`${__dirname}/../tmp/testing-driver`, {
+  it('Json driver', async () => {
+    const driver = new JsonDriver(`${__dirname}/../tmp/testing-driver`, {
       flashInterval: 100
     })
-    let created = yield driver.create('User', {
+    const created = await driver.create('User', {
       username: 'okunishinishi'
     })
-    let created2 = yield driver.create('User', {
+    const created2 = await driver.create('User', {
       username: 'hoge'
     })
     ok(created2.id !== created.id)
     ok(created.id)
     equal(created.username, 'okunishinishi')
 
-    let one = yield driver.one('User', created.id)
+    const one = await driver.one('User', created.id)
 
     equal(String(created.id), String(one.id))
 
-    let updated = yield driver.update('User', one.id, {
+    const updated = await driver.update('User', one.id, {
       password: 'hogehoge'
     })
     equal(String(updated.id), String(one.id))
     equal(updated.password, 'hogehoge')
 
-    let list = yield driver.list('User', {
-      filter: { username: 'okunishinishi' }
+    const list = await driver.list('User', {
+      filter: {username: 'okunishinishi'}
     })
 
-    deepEqual(list.meta, { offset: 0, limit: 100, length: 1, total: 1 })
-    yield driver.flush()
-    list = yield driver.list('User', {
-      filter: { username: 'okunishinishi' }
-    })
-    deepEqual(list.meta, { offset: 0, limit: 100, length: 1, total: 1 })
+    deepEqual(list.meta, {offset: 0, limit: 100, length: 1, total: 1})
+    await driver.flush()
+    {
+      const list = await driver.list('User', {
+        filter: {username: 'okunishinishi'}
+      })
+      deepEqual(list.meta, {offset: 0, limit: 100, length: 1, total: 1})
+    }
 
-    let destroyed = yield driver.destroy('User', one.id)
+    const destroyed = await driver.destroy('User', one.id)
     equal(destroyed, 1)
-    let destroyed2 = yield driver.destroy('User', one.id)
+    const destroyed2 = await driver.destroy('User', one.id)
     equal(destroyed2, 0)
 
-    let resources = yield driver.resources()
-    equal(resources[ 0 ].name, 'User')
+    const resources = await driver.resources()
+    equal(resources[0].name, 'User')
 
-    equal((yield driver.list('User')).meta.total, 1)
-    yield driver.flush()
-    equal((yield driver.list('User')).meta.total, 1)
-    yield driver.drop('User')
-    equal((yield driver.list('User')).meta.total, 0)
-    yield driver.flush()
-    equal((yield driver.list('User')).meta.total, 0)
-  }))
+    equal((await driver.list('User')).meta.total, 1)
+    await driver.flush()
+    equal((await driver.list('User')).meta.total, 1)
+    await driver.drop('User')
+    equal((await driver.list('User')).meta.total, 0)
+    await driver.flush()
+    equal((await driver.list('User')).meta.total, 0)
 
-  it('Run clayDriverTests', () => co(function * () {
-    let dirname = `${__dirname}/../tmp/testing-driver-2`
+    await driver.close()
+  })
+
+  it('Run clayDriverTests', async () => {
+    const dirname = `${__dirname}/../tmp/testing-driver-2`
     rimraf.sync(dirname)
-    let driver = new JsonDriver(dirname, {
+    const driver = new JsonDriver(dirname, {
       flashInterval: 100
     })
-    yield clayDriverTests.run(driver)
-  }))
+    await clayDriverTests.run(driver)
 
-  it('From lump', () => co(function * () {
-    let dirname = `${__dirname}/../tmp/testing-lump-driver`
+    await driver.close()
+  })
+
+  it('From lump', async () => {
+    const dirname = `${__dirname}/../tmp/testing-lump-driver`
     rimraf.sync(dirname)
-    let driver = new JsonDriver(dirname, {
+    const driver = new JsonDriver(dirname, {
       flashInterval: 1
     })
-    let lump = clayLump('Lump01', {
+    const lump = clayLump('Lump01', {
       driver
     })
-    let Hoge = lump.resource('Hoge')
+    const Hoge = lump.resource('Hoge')
 
-    let count = yield Hoge.count()
+    const count = await Hoge.count()
 
-    let hoge01 = yield Hoge.create({ name: 'hoge01' })
+    const hoge01 = await Hoge.create({name: 'hoge01'})
     equal(hoge01.name, 'hoge01')
-    yield lump.driver.flush()
-    let hoge01AsOne = yield Hoge.one(hoge01.id)
+    await lump.driver.flush()
+    const hoge01AsOne = await Hoge.one(hoge01.id)
     equal(hoge01AsOne.name, 'hoge01')
 
-    equal(yield Hoge.count(), count + 1)
+    equal(await Hoge.count(), count + 1)
 
-    yield driver.close()
-  }))
+    await driver.close()
+  })
 })
 
 /* global describe, before, after, it */
